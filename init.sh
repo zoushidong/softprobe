61289916486b08970b5b68cca4ddfcbaf97ff0bc
shidong.zou <499504620@qq.com>
2016-05-20 21:48:50 +0800

    init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+export license_key='5o8Y4YPrNFovbsNLaiA9zaE18kgrPgzf'
 cd /home/linaro/probe; nohup python main.py &
 nohup /usr/local/bin/iperf -s &
 nohup /usr/local/bin/iperf -s -u &
